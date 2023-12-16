6ca428ba7e7d02d4a362022585e772624383d164
kevish-gawri-vinci <kevish.gawri@student.vinci.be>
2023-12-16 06:41:27 +0100

    cors allowed

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 const helmet = require('helmet');
 
 const corsOptions = {
-  origin: ['http://localhost:8080', 'https://e-baron.github.io'],
+  origin: ['http://localhost:8080', 'https://e-baron.github.io', 'https://kevish-gawri-vinci.github.io/'],
 };
 
 const usersRouter = require('./routes/users');
